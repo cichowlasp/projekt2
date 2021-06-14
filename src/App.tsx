3d28491c86dea1c46e801c851d955c3eab04840c
Piotr Cichowlas <cichowlasp@Piotrs-iMac-Pro.local>
2021-06-14 14:04:48 +0200

    some font adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import { lightPallete, darkPallete } from './utils/colors';
 import { ThemeContext } from './Components/Context/DarkModeProvider';
 const App = () => {
 	const { darkMode } = useContext(ThemeContext);
-	const defaultFontSize = 8;
+	const defaultFontSize = 16;
 	const theme = {
 		colors: darkMode ? darkPallete : lightPallete,
 	};
 
 	const GlobalStyle = createGlobalStyle<{ bgColor: string }>`
   html {
-    font-size: ${defaultFontSize};
+    font-size: ${defaultFontSize}px;
     background-color: ${(props) => props.bgColor};
+	@media (min-width: 2000px) {
+		font-size: ${defaultFontSize * 2}px;
+		}
   }
+
   body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
